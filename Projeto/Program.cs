2d5bb9763768a19ab3a36a18448a3a97d8d820cd
Nathalia Barcelos <nathalia.barcelos@autoglass.com.br>
2024-11-26 19:29:56 -0300

    Rotina do botão ENVIAR

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -19,5 +19,28 @@ namespace Projeto_2_AED2
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        //Evento ao click no botao
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var text = this.richTextBox1.Text;
+            var palavras = text.Split(" "); //Divide em palavras
+            //verificando existencia no dic 
+            foreach(var palavra in palavras)
+            {
+                if (!await checkDicionario(palavra))
+                {
+                    adcionaDicionario(palavra);
+                }
+            }
+            //Definindo fonte padrao no dic
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionFont = new Font("Arial", (float)9);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
